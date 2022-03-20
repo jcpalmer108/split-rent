b57e06ddfdc87ec1851a4ef743e2b03fbf440177
jcpalmer108 <61334397+jcpalmer108@users.noreply.github.com>
2022-03-20 14:31:28 -0700

    line styles in apartment

--- src/views/Apartment/Apartment.js
+++ src/views/Apartment/Apartment.js
@@ -5,36 +5,37 @@ import "./Apartment.scss";
 import RoomDetails from "./components/RoomDetails.js";
 import RoommateDetails from "./components/RoommateDetails.js";
 import SpaceDetails from "./components/SpaceDetails.js";
-import ProgressBar from "../../components/ProgressBar/ProgressBar.js";
+// import ProgressBar from "../../components/ProgressBar/ProgressBar.js";
 
 export function Apartment() {
   const rent = useSelector((state) => state.rent);
   const roommate = useSelector((state) => state.roommate);
   const bedroom = useSelector((state) => state.bedroom);
   const bathroom = useSelector((state) => state.bathroom);
-  // const [showContinue, setShowContinue] = useState(false);
+  const [disableContinue, setDisableContinue] = useState(true);
   const [showRoommateDetails, setShowRoommateDetails] = useState(false);
+  const [showSpaceDetails, setShowSpaceDetails] = useState(false);
   const [areaChoice, setAreaChoice] = useState('length-width');
 
-  // useEffect(() => {
-  //   evaluateShowContinue();
-  // }, [rent, roommate, bedroom, bathroom])
-
-  // function evaluateShowContinue () {
-  //   if(rent > 0 && roommate > 0 && bedroom > 0 && bathroom > 0) {
-  //     setShowContinue(true)
-  //   } else {
-  //     setShowContinue(false)
-  //   }
-  // }
-
-  console.log("")
   useEffect(() => {
-    if(bedroom > 0 && bathroom > 0) {
-      setShowRoommateDetails(true);
-    }
+    evaluateShowContinue();
+  }, [rent, roommate, bedroom, bathroom])
 
+  function evaluateShowContinue () {
+    if(rent > 0 && roommate > 0 && bedroom > 0 && bathroom > 0) {
+      setDisableContinue(false)
+    } else {
+      setDisableContinue(true)
+    }
+  }
+
+  useEffect(() => {
+    if(bedroom > 0 && bathroom > 0) setShowRoommateDetails(true);
   }, [bedroom, bathroom])
+
+  useEffect(() => {
+    if(rent > 0 && roommate > 0) setShowSpaceDetails(true);
+  }, [rent, roommate])
 
   const handleAreaChoice = (event) => {
     setAreaChoice(event.target.value);
@@ -56,16 +57,23 @@ export function Apartment() {
   return (
     <div className="apartment">
       <form onSubmit={onSubmit}>
-        {/* <ProgressBar /> */}
         <h1>Tell me about your space</h1>
-        <RoomDetails options={options} />
-        <Collapse in={showRoommateDetails}>
-          <RoommateDetails options={options} />
-        </Collapse>
-        {/* <SpaceDetails areaChoice={areaChoice} onChange={handleAreaChoice} /> */}
-        {/* <Button disabled={!showContinue} type="submit" variant="contained">
-          Continue
-        </Button> */}
+          <div className="line">
+            <RoomDetails options={options} />
+          </div>
+          <div className="line">
+            <Collapse in={showRoommateDetails}>
+              <RoommateDetails className="line" options={options} />
+            </Collapse>
+          </div>
+          <div>
+            <Collapse in={showSpaceDetails}>
+              <SpaceDetails className="line" areaChoice={areaChoice} onChange={handleAreaChoice} />
+              <Button className="line" disabled={disableContinue} type="submit" variant="contained">
+                Continue
+              </Button>
+            </Collapse>
+          </div>
       </form>
     </div>
   );
