917645ddc1de9cbbb5f6daa88c086dc2205e590f
jcpalmer108 <61334397+jcpalmer108@users.noreply.github.com>
2022-03-20 13:42:41 -0700

    finished space details

--- src/views/Apartment/components/SpaceDetails.js
+++ src/views/Apartment/components/SpaceDetails.js
@@ -4,6 +4,7 @@ import { Select, MenuItem } from "@mui/material";
 export default function SpaceDetails(props) {
   return (
     <div>
+      I currently know the{' '}
       <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
@@ -17,6 +18,7 @@ export default function SpaceDetails(props) {
         <MenuItem value="length-width">Length & Width</MenuItem>
         <MenuItem value="squarefoot">Squarefootage</MenuItem>
       </Select>
+      {' '}of my new space.
     </div>
   )
 }
